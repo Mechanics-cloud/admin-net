5ab598f1c5992fd50ce8591d021f4ca4a952b032
KvashninAlexander2023 <study_python@mail.ru>
2025-03-30 09:55:35 +0500

    chore: add clsx cn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+        pathname: '/**',
+        port: '',
+        protocol: 'https',
+        search: '',
+      },
+    ],
+  },
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
