c58748a5a05aca67a70674a5446ce0d0356855d3
dariametelitsa <109726679+dariametelitsa@users.noreply.github.com>
2025-04-21 19:23:48 +0300

    hot fix: add style (#10)

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,7 @@
 import { useTranslations } from 'next-intl'
 import React from 'react'
 import LocaleSwitcher from '@/src/shared/components/localeSwither/LocaleSwitcher'
-import { Card, Typography } from 'car-robots-library'
+import { Button, Card, Typography } from 'car-robots-library'
 
 export default function HomePage() {
   const t = useTranslations('HomePage')
@@ -11,6 +11,11 @@ export default function HomePage() {
     <Card>
       <Typography variant={'h1'}>{t('title')}</Typography>
       <LocaleSwitcher />
+      <p>Уменьшай экран</p>
+      <div className={'bg-accent-100 md:bg-warning-500'}>1</div>
+      <div className={'bg-accent-200 md:bg-white'}>2</div>
+      <div className={'bg-accent-300 md:bg-warning-500 mb-6'}>3</div>
+      <Button variant={'secondary'}>Click</Button>
     </Card>
   )
 }
