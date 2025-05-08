4bbeb1a62da92d251f76ac07d9de20d601d61c9d
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-05-08 18:50:40 +0200

    IN-307 fix: change after merge

--- src/app/[locale]/(app)/page.tsx
+++ src/app/[locale]/(app)/page.tsx
@@ -1,3 +1,34 @@
-export default function HomePage() {
-  return <div>Home Page</div>
+'use client'
+import { Pagination, Select } from 'car-robots-library'
+import { useTranslations } from 'next-intl'
+import { TextField } from '@/src/shared'
+
+export default function UsersPage() {
+  const t = useTranslations('UsersPage')
+  return (
+    <div className={'text-light-100'}>
+      <div className={'flex justify-between max-w-[972px]'}>
+        <TextField
+          type={'search'}
+          label={''}
+          placeholder={'Search'}
+        />
+        <Select
+          placeholder={t('notSelected')}
+          className={'min-w-[234px] h-9'}
+        >
+          children
+        </Select>
+      </div>
+      <div className={'mt-9 custom-pagination'}>
+        <Pagination
+          currentPage={1}
+          onPageChange={() => {}}
+          onPageSize={() => {}}
+          pageSize={8}
+          totalItemsCount={100}
+        />
+      </div>
+    </div>
+  )
 }
