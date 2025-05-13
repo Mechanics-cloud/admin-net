951337854e3e0a8c348c1bb08506c001ca2dbba3
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-05-13 14:21:05 +0200

    IN-307 feat: add a table with a row for user list

--- src/app/[locale]/(app)/page.tsx
+++ src/app/[locale]/(app)/page.tsx
@@ -2,16 +2,19 @@
 import { Pagination, Select } from 'car-robots-library'
 import { useTranslations } from 'next-intl'
 import { TextField } from '@/src/shared'
+import { Typography } from 'car-robots-library'
+import Link from 'next/link'
 
 export default function UsersPage() {
   const t = useTranslations('UsersPage')
   return (
-    <div className={'text-light-100'}>
-      <div className={'flex justify-between max-w-[972px]'}>
+    <div className={'text-light-100 pt-12 pr-16'}>
+      <div className={'flex gap-[94px]'}>
         <TextField
           type={'search'}
           label={''}
           placeholder={'Search'}
+          className={'w-full'}
         />
         <Select
           placeholder={t('notSelected')}
@@ -20,6 +23,34 @@ export default function UsersPage() {
           children
         </Select>
       </div>
+      <table className='w-full text-left'>
+        <thead className='bg-dark-500 h-[48px]'>
+          <tr className='px-6'>
+            <th className='px-6'>
+              <Typography variant={'bold14'}>User ID</Typography>
+            </th>
+            <th className='px-6'>
+              <Typography variant={'bold14'}>Username</Typography>
+            </th>
+            <th className='px-6'>
+              <Typography variant={'bold14'}>Profile link</Typography>
+            </th>
+            <th className='px-6'>
+              <Typography variant={'bold14'}>Date added</Typography>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className=' h-[48px]'>
+            <td className='px-6'>21331QErQe21</td>
+            <td className='px-6'>Ivan Yakymenko</td>
+            <td className='px-6'>
+              <Link href='/profile'>Ivan.sr.yakimenko</Link>
+            </td>
+            <td className='px-6'>12.12.2022</td>
+          </tr>
+        </tbody>
+      </table>
       <div className={'mt-9 custom-pagination'}>
         <Pagination
           currentPage={1}
