ef31741783ff22fdc2a9772c37e280e6cbc67950
KvashninAlexander2023 <study_python@mail.ru>
2025-05-25 10:54:03 +0500

    IN-317 ref: add useGetFotos

--- src/_pages/uploadedFotos/ui/UploudedFotos.tsx
+++ src/_pages/uploadedFotos/ui/UploudedFotos.tsx
@@ -2,26 +2,31 @@
 
 import { useQuery } from '@apollo/client'
 import { GET_USER_FOTOS } from '../api/request'
-import Image from 'next/image'
-import { cn } from '@/src/shared'
+import { cn, responseErrorHandler } from '@/src/shared'
 import { useInView } from 'react-intersection-observer'
 import { useCallback, useEffect, useRef } from 'react'
+import NotContent from '../../404/NotContent'
+import { Stub } from '@/src/shared/components/stub'
+import { CircleLoader } from 'car-robots-library'
+import { useTranslations } from 'next-intl'
+import { FallbackImage } from '@/src/shared/components/fallbackImage'
 
 type Props = {
   userId: string
 }
 
 export default function UploadedFotos({ userId }: Props) {
-  // const [state, setState] = useState()
   const refStop = useRef<boolean>(false)
   const refEnd = useRef<number>(0)
   const refPrevEnd = useRef<number>(0)
+
+  const t = useTranslations('NoPost')
 
   const {
     data: uploadedPhotos,
     fetchMore,
     loading,
-    previousData,
+    error,
   } = useQuery(GET_USER_FOTOS, {
     variables: { Id: +userId, endCursorId: 0 },
     notifyOnNetworkStatusChange: true,
@@ -36,16 +41,12 @@ export default function UploadedFotos({ userId }: Props) {
     }
   }
 
-  // console.log('loading', loading)
-  console.log('previousData', previousData)
-
   const { ref, inView } = useInView({
     delay: 1000,
     initialInView: false,
     threshold: 0.5,
+    rootMargin: '200px 0px',
   })
-
-  // console.log('inView', inView)
 
   const handleLoadMore = useCallback(() => {
     const endCursorId = uploadedPhotos?.getPostsByUser?.items?.at(-1)?.id
@@ -72,36 +73,56 @@ export default function UploadedFotos({ userId }: Props) {
     }
   }, [inView, handleLoadMore])
 
+  if (error) {
+    responseErrorHandler(error)
+    return <NotContent />
+  }
+
   return (
     <>
       {uploadedPhotos?.getPostsByUser?.items?.length ? (
-        <div
-          className={cn(
-            'mt-9 grid gap-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 w-full h-full z-0'
-          )}
-        >
-          {uploadedPhotos.getPostsByUser.items.map((item, index) => (
-            <div
-              className='relative'
-              key={`${item.id}${index}`}
-            >
-              <Image
-                alt='image'
-                className='w-full h-auto object-cover'
-                height={228}
-                priority
-                src={item.url || ''}
-                width={342}
-              />
-            </div>
-          ))}
+        <>
           <div
-            className='w-full h-30 '
+            className={cn(
+              'mt-9 grid gap-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 w-full h-full z-0'
+            )}
+          >
+            {uploadedPhotos.getPostsByUser.items.map((item, index) => (
+              <div
+                className='relative'
+                key={`${item.id}${index}`}
+              >
+                {item.url && (
+                  <FallbackImage
+                    src={item.url}
+                    height={228}
+                    width={342}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+          <div
+            className='w-full h-1'
             ref={ref}
           ></div>
+          {loading && (
+            <CircleLoader
+              className={'w-full h-[114px] flex justify-center items-center'}
+            />
+          )}
+        </>
+      ) : loading ? (
+        <div className='mx-auto w-fit mt-10'>
+          <CircleLoader />
         </div>
       ) : (
-        <div>Постов нет</div>
+        <Stub
+          alt={t('alt')}
+          title={t('defaultText')}
+          variant='h1'
+          imageClassName='mt-10'
+        />
       )}
     </>
   )
