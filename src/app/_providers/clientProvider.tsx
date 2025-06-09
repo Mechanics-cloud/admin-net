1e2fa710a45006faf4ebbd20a1d8d37d54b7b8b6
Ochirov Bair <bairochirovdev@gmail.com>
2025-06-09 15:36:09 +0800

    IN-325 add deploy files (#22)
    * feat: add deploy files, refactor gitignore and add a script in package.json
    
    * feat: add jest
    
    * feat: add jest
    
    * feat: add jest
    
    * refactor: refactor package.json
    
    * refactor: docker file
    
    * fix: fix import
    
    * fix: fix nginx.conf because the server does not return Access-Control-Allow-Origin header (CORS)
    
    * fix: fix nginx.conf because the server does not return Access-Control-Allow-Origin header (CORS)
    
    * fix: fix nginx.conf because the server does not return Access-Control-Allow-Origin header (CORS)
    
    * test nginx.conf
    
    * refactor: revert to default nginx.conf
    
    * refactor: regenerate graphql types
    
    * refactor: regenerate graphql types
    
    * refactor: add HomePage to i18n messages for LocaleSwitcher.tsx

--- src/app/_providers/clientProvider.tsx
+++ src/app/_providers/clientProvider.tsx
@@ -2,7 +2,7 @@
 
 import { ApolloProvider } from '@apollo/client'
 import client from '@/src/shared/apolloClient/apolloClient'
-import React from 'React'
+import React from 'react'
 import AuthChecker from '@/src/features/auth/model/authChecker'
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
